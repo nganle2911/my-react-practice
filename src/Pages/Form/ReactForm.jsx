1242522a2ce0f9ce7c882435aedb185340f7cbd1
Ngan LE <ngan.ltkn2911@gmail.com>
2023-03-15 21:58:21 +0100

    Write a function named handleChangeInput() to get input after typying user

--- src/Pages/Form/ReactForm.jsx
+++ src/Pages/Form/ReactForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 export default class ReactForm extends Component {
-    
+
   state = {
     formValue: {
       id: "",
@@ -13,11 +13,31 @@ export default class ReactForm extends Component {
     },
   };
 
+    // Hàm xử lý obSutmit cho form 
   handleSubmit = (e) => {
     // Ngăn sự kiện reload browser
     e.preventDefault();
     console.log("submit");
   };
+
+    // Hàm xử lý input khi user nhập vào 
+  handleChangeInput = (e) => {
+    let value = e.target.value; 
+    let name = e.target.name; 
+    
+    // Lấy object formValue ra xử lý riêng 
+    let newFormValue = this.state.formValue; 
+    // Thay đổi state sau khi user nhập vào input - dùng dynamic key cho [name]
+    newFormValue[name] = value; 
+    // setState 
+    this.setState({
+        formValue: newFormValue
+    }, () => {
+        console.log(this.state); 
+    })
+
+    console.log(name, value);
+  }
 
   render() {
     return (
@@ -30,21 +50,21 @@ export default class ReactForm extends Component {
               <div className="col-6">
                 <div className="form-group">
                   <p>ID</p>
-                  <input className="form-control" name="id" />
+                  <input className="form-control" name="id" onInput={this.handleChangeInput} />
                 </div>
                 <div className="form-group">
                   <p>Name</p>
-                  <input className="form-control" name="name" />
+                  <input className="form-control" name="name" onInput={this.handleChangeInput} />
                 </div>
                 <div className="form-group">
                   <p>Price</p>
-                  <input className="form-control" name="price" />
+                  <input className="form-control" name="price" onInput={this.handleChangeInput} />
                 </div>
               </div>
               <div className="col-6">
                 <div className="form-group">
                   <p>Image</p>
-                  <input className="form-control" name="image" />
+                  <input className="form-control" name="image" onInput={this.handleChangeInput} />
                 </div>
                 <div className="form-group">
                   <p>Product Type</p>
@@ -56,7 +76,7 @@ export default class ReactForm extends Component {
                 </div>
                 <div className="form-group">
                   <p>Description</p>
-                  <input className="form-control" name="description" />
+                  <input className="form-control" name="description" onInput={this.handleChangeInput} />
                 </div>
               </div>
             </div>
