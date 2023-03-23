ffbd4ea7d27ced058db166c6d184faa65c85cc8f
Ngan LE <ngan.ltkn2911@gmail.com>
2023-03-23 22:15:36 +0100

    Practice Mounting stage

--- src/Pages/ReactLifeCycle/ReactLifeCycle.jsx
+++ src/Pages/ReactLifeCycle/ReactLifeCycle.jsx
@@ -1,9 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class ReactLifeCycle extends Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+    this.state = {
+      number: 1,
+    };
+  }
+
+  // Hàm này dùng để xử lý state trước khi render ra giao diện
+  static getDerivedStateFromProps(newProps, currentState) {
+    console.log("getDerivedStateFromProps");
+    return {
+      number: 2,
+    }; //return null là state giữ nguyên
+  }
+
   render() {
+    console.log("render constructor");
     return (
-      <div>ReactLifeCycle</div>
-    )
+      <div className="container">
+        <h3>number: {this.state.number}</h3>
+      </div>
+    );
   }
 }
