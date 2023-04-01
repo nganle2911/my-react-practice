f050653062385bb82acb05e85845eff975deedf6
Ngan LE <ngan.ltkn2911@gmail.com>
2023-04-01 23:30:49 +0200

    Practise useState in Hook

--- note.jsx
+++ note.jsx
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { useState } from 'react'
 
-export class note extends Component {
-  render() {
-    return (
-      <div>note</div>
-    )
+const note = () => {
+  let [number, setNumber] = useState(10);
+  let [fontSize, setFontSize] = useState(16); 
+
+  const handleChangeFontSize = (fSize) => {
+    let newFontSize = fontSize + fSize;
+    setFontSize(newFontSize); 
   }
+
+  return (
+    <div className='container'>
+      <h3>Exercise 1: Increase / Decrease Number</h3>
+      <p style={{fontSize: fontSize}}>Reprehenderit aute amet et adipisicing amet ex nostrud nostrud laborum ullamco culpa aliquip.</p>
+      <button className='btn btn-primary mx-2' onClick={() => {
+        handleChangeFontSize(5); 
+      }}>+</button>
+      <button className='btn btn-primary' onClick={() => {
+        handleChangeFontSize(-5);
+      }}>-</button>
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => ({})
-
-const mapDispatchToProps = {}
-
-export default connect(mapStateToProps, mapDispatchToProps)(note)
+export default note
--- src/Hook/UseStateDemo/UseStateDemo.jsx
+++ src/Hook/UseStateDemo/UseStateDemo.jsx
@@ -7,6 +7,14 @@ const UseStateDemo = () => {
   let [number, setNumber] = useState(10); // useState returns 1 mảng như sau: [state, setState()]
   // console.log("number", number);
 
+  let [fontSize, setFontSize] = useState(16); 
+
+  const handleChangeFontSize = (fSize) => {
+    let newFontSize = fontSize + fSize;
+    // setState thì giá trị mới thay thế hoàn toàn giá trị cũ => render lại 
+    setFontSize(newFontSize); 
+  }
+
   return (
     <div className='container mt-3'>
       <h3>Exercise 1: Increase / Decrease Number</h3>
@@ -19,6 +27,17 @@ const UseStateDemo = () => {
       <button className='btn btn-success' onClick={() => {
         number -= 1;
         setNumber(number);
+      }}>-</button>
+
+      <hr />
+
+      <h3>Exercise 2: Increase / Decrease Font Size</h3>
+      <p style={{fontSize: fontSize}}>Voluptate ipsum irure est cillum aliquip consectetur. Ullamco commodo anim irure eiusmod sit dolor eu non dolor fugiat labore esse eu nulla. Pariatur aliquip in duis ullamco ea ullamco voluptate dolore in Lorem. Justo amet diam lorem amet eirmod duo dolores stet, ea ut magna accusam diam amet rebum sit takimata justo, lorem.</p>
+      <button className='btn btn-primary mx-2' onClick={() => {
+        handleChangeFontSize(1); 
+      }}>+</button>
+      <button className='btn btn-primary' onClick={() => {
+        handleChangeFontSize(-1);
       }}>-</button>
     </div>
   )
