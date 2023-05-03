2d8449f7e0416bdc59309b84ef545b6f9e0df80b
Ngan LE <ngan.ltkn2911@gmail.com>
2023-05-03 15:26:05 +0200

    HOC

--- src/HOC/People/Person1.jsx
+++ src/HOC/People/Person1.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Person1 = () => {
+
+    const [money, setMoney] = useState(10); 
+
   return (
     <div>
-        <h3>Jimmy is offering $10</h3>
-        <button className='btn btn-success'>Increase Money</button>
+        <h3>Jimmy is offering ${money}</h3>
+        <button className='btn btn-success' onClick={() => {
+            
+        }}>Increase Money</button>
     </div>
   )
 }
